f480842c927ad569712dace7237839bb04cb02d1
Patricio Pérez Pinto <patodeath@gmail.com>
2024-05-08 17:12:57 -0400

    feat: add foreign service and controller. update README.md file

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PacMan API
 
-## Endpoints
+<details>
+  <summary>Native packages endpoints</summary>
 
 ### Get installed packages (explicit)
 ```commandline
@@ -38,6 +39,52 @@ curl -i -X GET "http://localhost:8080/api/v1/native/packages/vlc"
 HTTP/1.1 404
 {"message":"Package 'vlcs' not found"}
 ```
+</details>
+
+
+<details>
+  <summary>Foreign packages endpoints</summary>
+
+### Get installed packages (explicit)
+```commandline
+curl -i -X GET "http://localhost:8080/api/v1/foreign/packages/installed/explicit"
+```
+
+### Get installed packages (explicit). Lite version (name and version)
+```commandline
+curl -i -X GET "http://localhost:8080/api/v1/foreign/packages/installed/explicit/lite"
+```
+
+### Get packages to upgrade
+```commandline
+curl -i -X GET "http://localhost:8080/api/v1/foreign/packages/upgrade?password=$rootPassword"
+```
+
+#### Response when root password is wrong
+```
+HTTP/1.1 400
+{"message": "Wrong root password"}
+```
+
+#### Response when no package to upgrade
+```
+HTTP/1.1 204
+```
+
+### Get installed package by name
+```commandline
+curl -i -X GET "http://localhost:8080/api/v1/foreign/packages/google-chrome"
+```
+
+#### Response when package not found
+```
+HTTP/1.1 404
+{"message":"Package 'vlcs' not found"}
+```
+</details>
+
+<details>
+  <summary>Group packages endpoints</summary>
 
 ### Get groups name
 ```commandline
@@ -53,3 +100,4 @@ curl -i -X GET "http://localhost:8080/api/v1/native/groups/{name}"
 ```commandline
 curl -i -X GET "http://localhost:8080/api/v1/native/groups/package/{name}"
 ```
+</details>
---
+++ src/main/java/org/pack/manager/api/service/impl/ForeignPackageServiceImpl.java
@@ -0,0 +1,79 @@
+package org.pack.manager.api.service.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.pack.manager.api.exception.PackageNotFoundException;
+import org.pack.manager.api.exception.WrongPasswordException;
+import org.pack.manager.api.mapper.LitePackageMapper;
+import org.pack.manager.api.mapper.PackageMapper;
+import org.pack.manager.api.mapper.UpgradePackageMapper;
+import org.pack.manager.api.model.Package;
+import org.pack.manager.api.model.*;
+import org.pack.manager.api.service.CommandRunner;
+import org.pack.manager.api.service.PackageService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ForeignPackageServiceImpl implements PackageService {
+
+    private final CommandRunner commandRunner;
+    private final PackageMapper packageMapper;
+    private final UpgradePackageMapper upgradePackageMapper;
+    private final LitePackageMapper litePackageMapper;
+
+    @Override
+    public List<Package> getExplicitInstalledPackages() {
+        CommandRequest commandRequest = new CommandRequest("pacman -Qmei");
+        CommandResult commandResult = commandRunner.exec(commandRequest);
+
+        List<String> output = commandResult.getOutput();
+
+        return packageMapper.map(output);
+    }
+
+    @Override
+    public List<LitePackage> getExplicitLiteInstalledPackages() {
+        CommandRequest commandRequest = new CommandRequest("pacman -Qme");
+        CommandResult commandResult = commandRunner.exec(commandRequest);
+
+        List<String> output = commandResult.getOutput();
+
+        return litePackageMapper.map(output);
+    }
+
+    @Override
+    public List<UpgradePackage> getUpgradePackages(String rootPassword) {
+        CommandRequest commandRequest = new CommandRequest("yay -Sy", rootPassword);
+        CommandResult commandResult = commandRunner.exec(commandRequest);
+
+        if (commandResult.isNotSuccess()) {
+            throw new WrongPasswordException();
+        }
+
+        commandRequest = new CommandRequest("yay -Qum");
+        commandResult = commandRunner.exec(commandRequest);
+
+        List<String> output = commandResult.getOutput();
+
+        return upgradePackageMapper.map(output);
+    }
+
+    @Override
+    public Package getPackageBy(String name) {
+        try {
+            CommandRequest commandRequest = new CommandRequest("pacman -Qmei " + name);
+            CommandResult commandResult = commandRunner.exec(commandRequest);
+
+            List<String> output = commandResult.getOutput();
+
+            return packageMapper.mapToOne(output);
+        } catch (PackageNotFoundException ex) {
+            throw new PackageNotFoundException("Package '" + name + "' not found");
+        }
+    }
+
+}
