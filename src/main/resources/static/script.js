305f5ac5247bb5b5523b43f2c1e9090926c43ced
Patricio Pérez Pinto <patodeath@gmail.com>
2024-05-07 23:56:37 -0400

    feat: add groups features in index.html

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -348,3 +348,76 @@ function generatePackageTable(package) {
 
     return tableHTML;
 }
+
+// Obtener los botones por su id
+const getGroupsBtn = document.getElementById('getGroupsBtn');
+const getPackagesByGroupBtn = document.getElementById('getPackagesByGroupBtn');
+
+// Agregar event listener a cada botón
+getGroupsBtn.addEventListener('click', getGroups);
+getPackagesByGroupBtn.addEventListener('click', getPackagesByGroup);
+
+function handleResponse(response) {
+    const responseDiv = document.getElementById('response');
+    responseDiv.innerHTML = `<pre>${response}</pre>`;
+}
+
+function getGroups() {
+    fetch('http://localhost:8080/api/v1/pacman/groups')
+        .then(response => response.json())
+        .then(data => handleGetGroupsResponse(data))
+        .catch(error => console.error('Error:', error));
+}
+
+function getPackagesByGroup() {
+    const groupName = prompt("Enter group name:");
+    if (groupName) {
+        fetch(`http://localhost:8080/api/v1/pacman/groups/${groupName}`)
+            .then(response => {
+                if (response.status === 404) {
+                    return response.text().then(errorMessage => {
+                        const errorJson = JSON.parse(errorMessage);
+                        const errorMessageHTML = `Error: ${errorJson.message}`;
+                        const responseDiv = document.getElementById('response');
+                        responseDiv.innerHTML = errorMessageHTML;
+                    });
+                } else {
+                    return response.json();
+                }
+            })
+            .then(data => handlePackagesByGroupResponse(data))
+            .catch(error => console.error('Error:', error));
+    }
+}
+
+function handleGetGroupsResponse(response) {
+    const groupNames = response.groupNames;
+    const responseDiv = document.getElementById('response');
+    const html = generateGroupList(groupNames);
+    responseDiv.innerHTML = `<pre>${html}</pre>`;
+}
+
+function generateGroupList(groupNames) {
+    let listHTML = "<h3>PacMan Groups:</h3><ul>";
+    groupNames.forEach(groupName => {
+        listHTML += `<li>${groupName}</li>`;
+    });
+    listHTML += "</ul>";
+    return listHTML;
+}
+
+function handlePackagesByGroupResponse(response) {
+    const packages = response.packages;
+    const responseDiv = document.getElementById('response');
+    const html = generatePackageList(packages);
+    responseDiv.innerHTML = `<pre>${html}</pre>`;
+}
+
+function generatePackageList(packages) {
+    let listHTML = "<h3>Packages in Group:</h3><ul>";
+    packages.forEach(package => {
+        listHTML += `<li>${package}</li>`;
+    });
+    listHTML += "</ul>";
+    return listHTML;
+}
