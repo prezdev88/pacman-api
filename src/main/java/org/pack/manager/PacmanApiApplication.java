85d5f2c63dffdf83f349328680ccc901dc143202
Patricio Pérez Pinto <patodeath@gmail.com>
2024-05-09 00:31:01 -0400

    refactor: rename main class (DemoApplication to PacmanApiApplication). Delete tests

--- src/main/java/org/pack/manager/PacmanApiApplication.java
+++ src/main/java/org/pack/manager/PacmanApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PacmanApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PacmanApiApplication.class, args);
 	}
 
 }
